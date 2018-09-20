0fd27ed9342bd3ff751376620f2e26f8cf1683ca
Toukir Ahamed Pigeon <ToukirAhamedPigeon@users.noreply.github.com>
2018-09-20 09:48:02 +0600

    Update pdf.php

--- ci_pdf/application/modules/pdf/controllers/pdf.php
+++ ci_pdf/application/modules/pdf/controllers/pdf.php
@@ -11,21 +11,8 @@ class Pdf extends MY_Controller {
 
 	public function index()
 	{
-		// $filename = time()."_pdf.pdf";
-
-		// // $html = $this->load->view('unpaid_voucher',$data,true);
-		// $html = $this->load->view('pdf_v',"",true);
-
-		// // unpaid_voucher is unpaid_voucher.php file in view directory and $data variable has infor mation that you want to render on view.
-
-		// $this->load->library('mpdf');
-
-		// $this->m_pdf->pdf->WriteHTML($html);
-
+		
 		// //download it D save F.
-
-		// $this->m_pdf->pdf->Output("./uploads/".$filename, "F");
-
 		$defaultConfig = (new Mpdf\Config\ConfigVariables())->getDefaults();
 		$fontDirs = $defaultConfig['fontDir'];
 
